5c83038627ebdd8509f55eb51e088033d5f60b4c
Nartallax <nartallax@yandex-team.ru>
2017-05-02 20:21:04 +0300

    Little

--- algo/starts_of_sequences_without_particular_element.js
+++ algo/starts_of_sequences_without_particular_element.js
@@ -5,6 +5,8 @@
 // what element is "bad" is determined by isBad function
 module.exports = (s, seqSize, isBad) => {
 	var block = 0;
+	
+	// FIXME: bad usage of transaction()
 	var start = s.find(x => !isBad(x) && !s.transaction().take(seqSize - 1).exists(isBad))
 	return [start].stream().add(
 		s.filter(x => isBad(x + seqSize - 1)? ((block = seqSize), false): block-- <= 0)
